958ee84346002f741ef3ee1318059690ce250ac3
grdn <grdn@afba52b9-b8af-4cd2-b23a-b54a9f54f5a5>
2012-07-15 22:11:28 +0000

    - Currently building enemy skill list, which will get many nice features =)

--- Core.lua
+++ Core.lua
@@ -10,6 +10,7 @@ _G.PT=PT;
 -- pets nach stufe und qualität sortieren
 -- Vorschläge, welches Setup das Pet schlagen könnte
 
+LibStub("iLib"):Register(AddonName, nil, PT);
 
 ----------------------
 -- Variables
@@ -26,25 +27,12 @@ function PT:Boot()
 	if( not _G.IsAddOnLoaded("Blizzard_PetJournal") ) then
 		_G.LoadAddOn("Blizzard_PetJournal");
 	end
+	
+	self:RegisterEvent("PET_BATTLE_OPENING_DONE", "PetBattleStart");
+	self:RegisterEvent("PET_BATTLE_OVER", "PetBattleStop");
+	self:RegisterEvent("PET_BATTLE_CLOSE", "PetBattleStop");
 end
 PT:RegisterEvent("PLAYER_ENTERING_WORLD", "Boot");
-
-
-
-
-
-function PT:Compare(petType, enemyType)
-	return _G.C_PetBattles.GetAttackModifier(petType, enemyType);
-end
-
-function PT:ComparyByAbilityID(abilityID, enemyType)
-	local _, name, icon, _, _, _, petType, noStrongWeakHints = _G.C_PetBattles.GetAbilityInfoByID(abilityID);
-	if( noStrongWeakHints ) then
-		return 1, name, icon;
-	else
-		return self:Compare(petType, enemyType), name, icon;
-	end
-end
 
 ---------------------
 -- Utility
@@ -68,6 +56,92 @@ local function damage_icon(modifier, size)
 		return "|TInterface\\PetBattles\\BattleBar-AbilityBadge-Neutral:"..size..":"..size.."|t";
 	end
 end
+
+function PT:Compare(petType, enemyType)
+	return _G.C_PetBattles.GetAttackModifier(petType, enemyType);
+end
+
+function PT:ComparyByAbilityID(abilityID, enemyType)
+	local _, name, icon, _, _, _, petType, noStrongWeakHints = _G.C_PetBattles.GetAbilityInfoByID(abilityID);
+	if( noStrongWeakHints ) then
+		return 1, name, icon;
+	else
+		return self:Compare(petType, enemyType), name, icon;
+	end
+end
+
+---------------------------------------------------------
+-- BATTLE FRAME
+---------------------------------------------------------
+
+local abilityIDs, abilityLevels = {}, {};
+local enemyAbilitys = {};
+
+_G.E = enemyAbilitys;
+
+function PT:PetBattleStart()
+	local species, id, name, icon, maxCD, desc;
+	local petName, petIcon, petType, creatureID;
+	
+	for i = 1, 3 do
+		species = _G.C_PetBattles.GetPetSpeciesID(_G.LE_BATTLE_PET_ENEMY, i);
+		_G.C_PetJournal.GetPetAbilityList(species, abilityIDs, abilityLevels);
+		petName, petIcon, petType, creatureID = _G.C_PetJournal.GetPetInfoBySpeciesID(species);
+		
+		enemyAbilitys[i] = {
+			name = petName,
+			icon = petIcon,
+			type = petType,
+		};
+		
+		for j, ability in pairs(abilityIDs) do
+			id, name, icon, maxCD, desc = _G.C_PetBattles.GetAbilityInfoByID(ability);
+			
+			table.insert(enemyAbilitys[i], {abilityLevels[j], _G.C_PetBattles.GetAbilityInfoByID(ability)});
+		end
+	end
+
+	local tip = self:GetTooltip("EnemySkills", "UpdateEnemySkills");
+	tip:SetPoint("TOPRIGHT", _G.UIParent, "RIGHT", 0, 300);
+	tip:Show();
+	
+	_G.WatchFrame:Hide();
+end
+
+function PT:PetBattleStop()
+	self:GetTooltip("EnemySkills"):Release();
+	
+	_G.WatchFrame:Show();
+end
+
+function PT:UpdateEnemySkills(tip)
+	tip:Clear();
+	tip:SetColumnLayout(2, "LEFT", "LEFT");
+	
+	_G.wipe(abilityIDs);
+	_G.wipe(abilityLevels);
+	
+	local id, name, icon, maxCD, desc;
+	
+	for i = 1, 3 do
+		local species = _G.C_PetBattles.GetPetSpeciesID(_G.LE_BATTLE_PET_ENEMY, i);
+		
+		_G.C_PetJournal.GetPetAbilityList(species, abilityIDs, abilityLevels);
+		
+		for _, ability in pairs(abilityIDs) do
+			id, name, icon, maxCD, desc = C_PetBattles.GetAbilityInfoByID(ability);
+			
+			tip:AddLine(
+				"|T"..icon..":24:24|t",
+				name
+			);
+		end
+	end
+end
+
+---------------------------------------------------------
+-- GAME TOOLTIP
+---------------------------------------------------------
 
 local function GameTooltip_hook(self)
 	local name, unit = self:GetUnit();
@@ -122,5 +196,4 @@ local function GameTooltip_hook(self)
 		end
 	end -- end for
 end
-
 GameTooltip:HookScript("OnTooltipSetUnit", GameTooltip_hook);
