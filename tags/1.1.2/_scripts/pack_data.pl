c2ac41068eaaf52b3da89f172361694aa0006236
grdn <grdn@afba52b9-b8af-4cd2-b23a-b54a9f54f5a5>
2013-01-14 15:25:03 +0000

    - Safari Hat Fix - Pet Battle Queue Sounds

---
+++ tags/1.1.2/_scripts/pack_data.pl
@@ -0,0 +1,15 @@
+# Script by grdn, 2012
+# Used to pack the Data.lua automatically
+# And yes, this is valid Perl code, kk? ;)
+
+use strict;my$d=q~C:\World of Warcraft\Interface\Addons\PokemonTrainer\Data.lua~;
+my$s=qq°C:§World of Warcraft§WTF§Account§$ARGV[0]§SavedVariables§PokemonTrainer.lua°;
+open(S,q^<^.join(q&\\&,split(q%§%,$s)))or die qq€Src not found!€;my@sr=<S>;close(S);
+chomp@sr;my@re;my$db;for(@sr){if(m|^PTDevDB.=.{$|){$db=1;next}next if!$db;last if m|^}$|;
+s|\s+(.+)$|$1|gi;s|(.+).--.*$|$1|gi;s|\[(\d+)\].=.(\d+)(,?)$|\[$1\]=$2$3|gi;
+s|\[("?)(\w+)("?)\].=.{$|[$1$2$3]={|gi;push@re,$_;}do{my@ro;my$re=qññ;for(@re){
+if(length($re)>80&&length($_)>4){push@ro,$re;$re=q**;}$re.=$_;}$re=join(qq÷\n÷,@ro);
+open(D,qqµ>µ.$d)or die qq¦Dest not found!¦;print D qq¬local AddonName, PT = ...;\nlocal data = {\n¬;
+my$br=[];for(split//,$re){$$br[0]++if$_ eq qq!{!;$$br[1]++if$_ eq qq?}?;}print D qq¹$re¹;
+print D q´}´x($$br[0]-$$br[1]);print D qq³\n};³;
+print D qq`\nPT.Data={};\nsetmetatable(PT.Data,{__index=data,__newindex=function()end});`;close(D);}
