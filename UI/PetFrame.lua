ac12ee14b9efad8300f3a5d9a9e827f5856ae153
grdn <grdn@afba52b9-b8af-4cd2-b23a-b54a9f54f5a5>
2014-11-09 10:43:00 +0000

    - Started to bring some UI to the mod - Moved Patchwerk.lua from Core to Patchwerk folder

---
+++ UI/PetFrame.lua
@@ -0,0 +1,9 @@
+local AddonName, PT = ...;
+local Pet = PT.PetClass;
+
+local Const, Battle = PT:GetComponent("Const", "Battle");
+
+-------------------------------------------------------------
+-- Pet related functions
+-------------------------------------------------------------
+
